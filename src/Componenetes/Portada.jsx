a5e6e9c479472391b65123b2638bc9a5a716d763
Gonza <gnzx89@gmail.com>
2024-10-10 13:55:32 -0300

    creando portada

---
+++ src/Componenetes/Portada.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Estilos/Portada.css';
+
+export const Portada = () => {
+  return (
+    <div className='container-portada'>
+        <img src="https://images01.nicepagecdn.com/c461c07a441a5d220e8feb1a/8ab91149eb155af29f11496c/d0f3edac-474b-42be-b569-946cd98464a7.jpg" alt="img-portada" className='img-portada'/>
+    </div>
+  )
+}
